4b114ca3fc0b32d736889f8962b1a4532fd1af7e
liu1315177581 <1315177581qq.com>
2016-10-27 10:57:25 +0800

    firstadd

---
+++ resume/data/userInfo.js
@@ -0,0 +1,35 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "刘永顺" , 
+    userPortrait:"images/icon/479E23AC376D4AF5D9520E7DD57924F2.jpg",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "1315177581",
+    userEmail:   "1315177581@qq.com",
+    motto:  "", 
+    userPhone: 13903485256,
+    userOrignPlace:  "山西省河津市" ,
+    userSeatPlace:  "北京市",
+    userAssessment: "学习能力强,理解能力强,性格开朗,为人和善",
+    userHobby: ["健身","爬山","电影"]
+}
+
